24fd99b18314840e9a1580fffbf36d29cb1a647b
Sobol0202 <67016865+Sobol0202@users.noreply.github.com>
2023-08-09 13:32:46 +0200

    Update LSS Karten Filter Hider.user.js

--- LSS Karten Filter Hider.user.js
+++ LSS Karten Filter Hider.user.js
@@ -1,14 +1,13 @@
 // ==UserScript==
 // @name        LSS Karten Filter Hider
-// @version     1.0.0
+// @version     1.0.1
 // @description Removes the Map Filter Button.
 // @author      MissSobol
 // @include     /^https?:\/\/(?:w{3}\.)?(?:operacni-stredisko\.cz|alarmcentral-spil\.dk|leitstellenspiel\.de|missionchief\.gr|(?:missionchief-australia|missionchief|hatakeskuspeli|missionchief-japan|missionchief-korea|nodsentralspillet|meldkamerspel|operador193|jogo-operador112|jocdispecerat112|dispecerske-centrum|112-merkez|dyspetcher101-game)\.com|missionchief\.co\.uk|centro-de-mando\.es|centro-de-mando\.mx|operateur112\.fr|operatore112\.it|operatorratunkowy\.pl|dispetcher112\.ru|larmcentralen-spelet\.se)\/?$/
 // @grant       none
 // ==/UserScript==
-
-var test = document.getElementsByClassName('leaflet-control-layers-toggle');
-  for (var index = 0; 
-       index < test.length; index++) 
-          { test[index].remove(); 
-}
+//Lieber Scriptnutzer,
+//schweren Herzens habe ich heute alle meine Scripte deaktiviert. Der Betreiber hat mir meinen Forenaccount gesperrt, will mir aber weder verraten warum, noch ist man gewillt in Diskurs mit mir zu treten. Da ich ohne einen Forenaccount nicht das Level an Support geben kann, welches ich für mich selber als erforderlich betrachte, habe ich mit sofortiger Wirkung alle meine Scripte deaktiviert.
+//Ich bedanke mich für 5 Jahre Zusammenarbeit im Forum. Danke an alle, die meine Scripte genutzt haben, Daumen dagelassen haben und Kommentare geschrieben haben.
+//Liebe Grüße und noch viel Spaß
+//Sobol
